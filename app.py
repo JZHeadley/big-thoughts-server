b4fb0b0f53d95b9ed2319635f3f515c142416516
Justin Miller <justintime145@gmail.com>
2019-02-17 04:09:18 -0500

    Porting api to flask.

--- app.py
+++ app.py
@@ -1,9 +1,53 @@
 from flask import Flask
 from flask_sockets import Sockets
 from flask_cors import CORS
+from . import  get_model
+from flask_sqlalchemy import SQLAlchemy
 
-from thoughtio import init_msg
 
+from .thoughtio import init_msg
+import json
+db = SQLAlchemy()
+class Message(db.Model):
+    time_stamp = db.Column(db.Date())
+    content = db.Column(db.String(512))
+    author = db.Column(db.String(10), db.ForeignKey('person.user_id'))
+    class_id = db.Column(db.Integer, db.ForeignKey('class.class_id'))
+    message_id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.String(10), db.ForeignKey('student.student_id'))
+
+class Class(db.Model):
+    class_id = db.Column(db.Integer, primary_key = True)
+    class_num = db.Column(db.String(30))
+    class_name = db.Column(db.String(80))
+
+
+class TA(db.Model):
+    ta_id = db.Column(db.String(10), db.ForeignKey('person.user_id'), primary_key = True)
+
+
+class Student(db.Model):
+    student_id = db.Column(db.String(10), db.ForeignKey('person.user_id'), primary_key = True)
+    phone_number = db.Column(db.String(30))
+
+
+class Class_TA(db.Model):
+    ta_id = db.Column(db.String(10), db.ForeignKey('person.user_id'), primary_key = True)
+    class_num = db.Column(db.Integer, db.ForeignKey('class.class_id'), primary_key = True)
+
+class Person(db.Model):
+    user_id = db.Column(db.String(10), primary_key = True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+
+
+
+def from_sql(row):
+    """Translates a SQLAlchemy model instance into a dictionary"""
+    data = row.__dict__.copy()
+    data['id'] = row.id
+    data.pop('_sa_instance_state')
+    return data
 DEBUG=True
 app = Flask(__name__)
 sockets = Sockets(app)
@@ -30,17 +74,20 @@ def hello():
 def get_user_by_ID(userID):
         return userID
 
-@app.route('/{taid}/classes', methods=["GET"])
+@app.route('/tas/{taid}/classes', methods=["GET"])
 def get_class_list_by_taid_handler(taid):
-        return getClassListByTaid(taid)
+        query = Session.query(Class_TA, Class).filter(Class_TA.class_num == Class.class_num).filter(Class_TA.ta_id == taid)
+        return builtin_list(map(from_sql, query.all()))
 
 @app.route('/ta/{taid}/classes', methods=["GET"])
 def populate_data_for_TA_handler(taid):
         return taid
 
-@app.route('/classes/{classID}/members', methods=["GET"])
+@app.route('/classes/<classID>', methods=["GET"])
 def get_class_members_handler(classID):
-        return classID
+        distinct = Message.query.filter_by(class_id == class_id).all()
+        query = Class.query.filter_by(user_id in distinct.user_id)
+        return builtin_list(map(from_sql, query.all()))
 
 @app.route('/classes/{classID}/{userID}/messages', methods=["GET"])
 def get_message_history_handler():
